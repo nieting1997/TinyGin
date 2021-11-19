f83d467d35278189f46ada3ab2ae352ab233c9aa
nieting <nieting@mac>
2021-11-19 17:39:42 +0800

    1

---
+++ trie.go
@@ -0,0 +1,73 @@
+package gee
+
+import "strings"
+
+// 树节点
+type node struct {
+	// 待匹配路由
+	pattern string
+	// 路由中的一部分
+	part string
+	// 子节点
+	children []*node
+	// 是否精确匹配
+	isWild bool
+}
+
+// 第一个匹配成功的节点
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// 所有匹配成功的节点
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+// 插入节点
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		// 都可匹配
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+// 匹配节点
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		res := child.search(parts, height+1)
+		if res != nil {
+			return res
+		}
+	}
+
+	return nil
+}
